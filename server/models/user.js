bbb9e8450bd6bf76f9d139a30e9400f6d7d80344
caloc <quangdv2802@gmail.com>
2024-09-16 09:15:06 +0700

    hash password

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose') // Erase if already required
-
+const bcrypt = require('bcrypt')
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
     firstName: {
@@ -51,6 +51,11 @@ var userSchema = new mongoose.Schema({
         type: String
     }
 }, { timestamps: true })
+
+userSchema.pre('save', async function (next) {
+    const salt = bcrypt.genSaltSync(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
 
 //Export the model
 module.exports = mongoose.model('User', userSchema)
