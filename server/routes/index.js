f09808bed0dfe4980cc00d0cd5bafeda908cd227
caloc <quangdv2802@gmail.com>
2024-09-20 22:32:48 +0700

    CRUD categories

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 const userRouter = require('../routes/user')
 const productRouter = require('../routes/product')
+const productCategoryRouter = require('../routes/productCategory')
 const { notFound, errHandler } = require('../middlewares/errorHandler')
 
 const initRoutes = (app) => {
     app.use('/api/user', userRouter)
     app.use('/api/product', productRouter)
+    app.use('/api/productCategory', productCategoryRouter)
 
 
     app.use(notFound)
