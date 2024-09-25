6804797b1292180425aa0cfb142c0c7e980e83c1
caloc <quangdv2802@gmail.com>
2024-09-25 15:50:48 +0700

    finish base BE

--- server/routes/order.js
+++ server/routes/order.js
@@ -2,7 +2,10 @@ const router = require('express').Router()
 const orderController = require('../controllers/order')
 const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken')
 
+router.get('/', [verifyAccessToken], orderController.getUserOrder)
+router.get('/admin', [verifyAccessToken, isAdmin], orderController.getAllOrders)
 router.post('/', [verifyAccessToken], orderController.createOrder)
+router.put('/status/:oid', [verifyAccessToken, isAdmin], orderController.updateStatus)
 
 
 module.exports = router
